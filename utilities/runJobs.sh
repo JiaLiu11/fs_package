cbd82a87f05a132ac58f13e17b625ce881830146
Jia Liu <liu.2053@osu.edu>
2014-05-22 13:10:35 -0400

    Bug fix: now enters utilities to run python script.

--- utilities/runJobs.sh
+++ utilities/runJobs.sh
@@ -13,7 +13,7 @@ for i in $(eval echo "{1..$1}"); do
 
 	cd $here
 	cp -r -a $here/main $here/nodes/node$i
-	cd $here/nodes/node$i
+	cd $here/nodes/node$i/utilities
 
 	screen -dmS jia$i
 	screen -S jia$i -p 0 -X stuff "python ./runcode.py$(printf \\r)";
