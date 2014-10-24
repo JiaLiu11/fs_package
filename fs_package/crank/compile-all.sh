9cf58ff34af9280ce4b95280b582b58d15893507
Jia Liu <liu.2053@osu.edu>
2014-10-24 13:56:34 -0400

    Replace crank folder.

---
+++ fs_package/crank/compile-all.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+(cd ../
+for ii in fs superMC VISHNew iS
+    do
+    (cd $ii; make; make clean)
+done
+
+echo "Compiling finished."
+echo "Next generate jobs using generate-jobs-XXX.sh."
+)
