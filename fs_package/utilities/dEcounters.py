cb05c410d81a2341e8aa035b919fe23d73819c1d
Jia Liu <liu.2053@osu.edu>
2015-03-19 09:34:29 -0400

    Merge dEcounters.py from iEBE package.

--- fs_package/utilities/dEcounters.py
+++ fs_package/utilities/dEcounters.py
@@ -18,7 +18,7 @@
 #   Jan 24, 2014     Read the 'chosen_particles_backup.dat' to maintain
 #    				 the correct matrix dimension. Commented dN calculation.
 
-import sys
+import sys,shutil
 from os import path, stat, getcwd
 import numpy as np
 
@@ -148,6 +148,22 @@ def dEdydphipSigmaFO(iSFolder, iSDataFolder, targetFolder, fileName):
 	return dEdy
 
 
+def getTotaldEdy(dEdyd2rdphipFile, edFile, normFactor, iSFolder, iSdataFolder, \
+        dEdphipOutName = 'dEdydphipThermal.dat',dEdphipHydroName = 'dEdydphipFO.dat'):
+    """
+    run two functions from dEcounters to get the total dEdy, also generate 
+    dEdydphip tables for out surface and hydro surface. 
+    """
+    dEdy_therm = dEdydphipSigmaThermal(dEdyd2rdphipFile,edFile,\
+        normFactor, iSdataFolder, dEdphipOutName)
+    dEdy_fo = dEdydphipSigmaFO(iSFolder, iSdataFolder,\
+        iSdataFolder, dEdphipHydroName)
+    # total energy 
+    totaldEdy = dEdy_therm + dEdy_fo
+    return totaldEdy 
+
+
+
 def dEdyTotalDecdat2(resultsFolder, fileName = 'decdat2.dat'):
 	"""
 	Calculate total energy from hydro freeze-out surface, i.e. file 'decdat2.dat'
@@ -257,6 +273,27 @@ def dEdytotalConstSurface(resultsFolder, fileName = 'constTauSurface.dat',dxdy=0
 
 	return dEdy
 
+def radialFlowFO(resultsFolder, fileName = 'decdat2.dat'):
+	"""
+	Calculate radial flow on freeze-out surface
+	formula: v_Sigma_fo=(\int_Sigma_fou^mu d^3sigma_mu v_perp)/(\int_Sigma_fo u^mu d^3sigma_mu)
+	"""
+	decdat2_file = path.join(resultsFolder, fileName)
+	if(stat(decdat2_file).st_size==0):  # empty decdat2 file, hydro does not run
+		return 0
+	decData_fo = np.loadtxt(decdat2_file)
+
+	vx_fo = decData_fo[:,4]
+	vy_fo = decData_fo[:,5]
+	gamma_fo = 1.0/np.sqrt(1.0 - vx_fo**2- vy_fo**2 )
+	ux_fo = decData_fo[:,4]*gamma_fo
+	uy_fo = decData_fo[:,5]*gamma_fo
+	vr_fo = np.sqrt(vx_fo**2+vy_fo**2)
+	udsigma = decData_fo[:,0]*(gamma_fo*decData_fo[:,1]+\
+		+ux_fo*decData_fo[:,2]+uy_fo*decData_fo[:,3])
+	vr = sum(udsigma*vr_fo)/sum(udsigma)
+	return vr
+
 
 
 
@@ -351,4 +388,133 @@ def getT0muFromiS(iSEOSFolder, iSDataFolder):
 			T0mu[j] += thermalParticleTbl[0, j+2]
 		i+=1
 	return T0mu[:]
+
+
+def doubleProjectPimn(decdat2_line):
+    """
+    Applying double projection operator on hydro dumped pi^munu.
+    Return the line with replaced pi^munu
+    """
+    gmn = np.mat([[1., 0., 0., 0.], \
+        [0., -1, 0., 0.],   \
+        [0., 0., -1., 0.],  \
+        [0., 0., 0., -1.]]) # metric
+    # get umu rank-1 matrix
+    vx_fo = decdat2_line[4]
+    vy_fo = decdat2_line[5]
+    gamma_fo = 1.0/np.sqrt(1.0 - vx_fo**2- vy_fo**2 )
+    ux_fo = decdat2_line[4]*gamma_fo
+    uy_fo = decdat2_line[5]*gamma_fo    
+    uz_fo = 0
+    umu = np.mat([gamma_fo, ux_fo, uy_fo, uz_fo])
+    # get pi^munu matrix
+    pimn = np.zeros([1, 7])
+    for i in range(1,7):
+        pimn[0,i-1] = decdat2_line[i+12] # pi00, pi01, pi02, pi11, pi12, pi22
+    pimn[0,-1] = decdat2_line[12] # pi33
+    pimn_input = np.mat([[pimn[0, 0], pimn[0, 1], pimn[0, 2], 0.],    \
+    [pimn[0, 1], pimn[0, 3], pimn[0, 4], 0.], \
+    [pimn[0, 2], pimn[0, 4], pimn[0, 5], 0], \
+    [0., 0., 0., pimn[0, 6]] ])
+
+    # apply double projection operator on pi^munu
+    pimn_ap = (gmn*gmn - umu.T*(umu*gmn))*(((gmn*gmn - umu.T*(umu*gmn))*pimn_input).T) \
+        -1.0/3.0*(gmn - umu.T*umu)*((np.trace(pimn_input*gmn)-(umu*gmn)*pimn_input*(gmn*umu.T)))[0,0]
+    # update pi^munu the decdat2_oneLine
+    decdat2_line[12] = pimn_ap[3,3]
+    decdat2_line[13] = pimn_ap[0,0]
+    decdat2_line[14] = pimn_ap[0,1]
+    decdat2_line[15] = pimn_ap[0,2]
+    decdat2_line[16] = pimn_ap[1,1]
+    decdat2_line[17] = pimn_ap[1,2]
+    decdat2_line[18] = pimn_ap[2,2]
+    return decdat2_line
+
+
+def preProcessDecdat2File(source_folder):
+    """
+    Pre-process input decdat2 file, replacing the
+    original decdat2.dat. Backup the original one.
+    """
+    decdat2_fileName = 'decdat2.dat' 
+    decdat2_file = path.join(source_folder, decdat2_fileName)
+    try:
+        decdat2_input = np.loadtxt(decdat2_file)
+    except:
+        print 'preProcessPimn: cannot read file: '+decdat2_file
+        sys.exit(-1)
+
+    # process file
+    if decdat2_input.ndim == 1: # only one line in decdat2.dat
+        decdat2_oneLine = decdat2_input
+        decdat2_output = doubleProjectPimn(decdat2_oneLine)
+    else:
+        decdat2_output = np.zeros((1, 20)) # prepare a zero line to accept the following data
+        decdat2_file_length = decdat2_input.shape[0]
+        for i in range(0, decdat2_file_length):
+            decdat2_oneLine = decdat2_input[i,:]
+            decdat2_oneLine_new = doubleProjectPimn(decdat2_oneLine)
+            decdat2_output = np.concatenate((decdat2_output,np.mat(decdat2_oneLine_new)))
+        decdat2_output = np.delete(decdat2_output, 0, axis=0) # delete the zero data in line 1
+
+    # back up the old decdat2.dat and save the new one
+    decdat2_backupFileName = 'decdat2_backup.dat'
+    decdat2_backupFile = path.join(source_folder, decdat2_backupFileName)
+    shutil.move(decdat2_file, decdat2_backupFile)
+    if decdat2_input.ndim == 1:
+        np.savetxt(decdat2_file, decdat2_output[None], fmt='%14.6E')
+    else:
+        np.savetxt(decdat2_file, decdat2_output, fmt='%14.6E')
+
+
+def calculateWn(folder, dEdphipOutName = 'dEdydphipThermal.dat',
+    dEdphipHydroName = 'dEdydphipFO.dat', outputFileName = 'wn_integrated_vndata.dat'):
+    """
+    This function calculate energy flow anisotropy omega_n (Wn) from the energy azimuthal distribution
+    at the hydro surface and the out surface.
+    It is the python version of part of the matlab script "" which calculates the same quantity.
+    The output Format is has 10 rows and 5 columns for order 1 to order 9
+    """
+    try:
+        # load phip table
+        phip_th = np.loadtxt(path.join(tables_location, 'phip_gauss_table.dat'))
+        phip_fo = np.loadtxt(path.join(tables_location, 'phi_gauss_table.dat'))
+        # load energy distribution
+        dEdydphip_fo_data = np.loadtxt(path.join(folder, dEdphipHydroName))
+        dEdydphip_th_data = np.loadtxt(path.join(folder, dEdphipOutName))
+    except:
+        print "calculateWn: no necessary files. Check iS/results Folder!\n"
+        return False
+
+    try:
+        # calculate total energy
+        wn_th_denominator = np.sum(dEdydphip_th_data*phip_th[::,1])
+        wn_fo_denominator = np.sum(dEdydphip_fo_data*phip_fo[::,1])
+    except:
+        print "calculateWn: dimensions do not match. Check phi and phip tables!\n"
+        return False
+
+    # pre-allocate space
+    wn_data = np.zeros((10, 6))
+
+    # loop over all orders
+    for iorder in range(0,10):
+        # numerators for energy at out surface
+        wn_th_numerator_real = np.sum(dEdydphip_th_data*np.cos(iorder*phip_th[:,0])*phip_th[:,1])
+        wn_th_numerator_img  = np.sum(dEdydphip_th_data*np.sin(iorder*phip_th[:,0])*phip_th[:,1])
+        # numerators for energy at hydro surface
+        wn_fo_numerator_real = np.sum(dEdydphip_fo_data*np.cos(iorder*phip_fo[:,0])*phip_fo[:,1])
+        wn_fo_numerator_img  = np.sum(dEdydphip_fo_data*np.sin(iorder*phip_fo[:,0])*phip_fo[:,1])
+        # calculate omega n
+        wn_real = (wn_th_numerator_real+wn_fo_numerator_real)/(wn_th_denominator+wn_fo_denominator+1e-18)
+        wn_img  = (wn_th_numerator_img + wn_fo_numerator_img)/(wn_th_denominator+wn_fo_denominator+1e-18)
+        wn_data[iorder, :] = np.array([iorder, (wn_th_numerator_real+wn_fo_numerator_real),
+                                               (wn_th_numerator_img +wn_fo_numerator_img),
+                                               wn_real, wn_img, np.sqrt(wn_real**2.0+wn_img**2.0)])
+    # save file to folder
+    savefileName = path.join(folder, outputFileName)
+    np.savetxt(savefileName, wn_data,fmt='%19.8e')
+    return True
+
+
 
